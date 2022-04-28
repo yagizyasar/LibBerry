5c3556db92850842ccda5e49ea3be17bdb439a44
saitcanbaskol <saitcan.baskol@ug.bilkent.edu.tr>
2022-04-28 20:16:46 +0300

    User model template incomplete

--- libberry/user/models.py
+++ libberry/user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    password = models.CharField(max_length=32,null=False)
+    name = models.CharField(max_length=32,null=False)
+    surname = models.CharField(max_length=32,null=False)
+    balance = models.DecimalField(max_digits=7,decimal_places=2)
+    email = models.EmailField(max_length=254,null=False)
+    
+    class Meta:
+        abstract = True
+    
