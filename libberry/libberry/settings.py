57360e6f80882da9e2525a161a4a1bcb9dfe9f95
saitcanbaskol <saitcan.baskol@ug.bilkent.edu.tr>
2022-04-28 18:52:56 +0300

    initilization done

--- libberry/libberry/settings.py
+++ libberry/libberry/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'libberry.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'libberrydb',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
