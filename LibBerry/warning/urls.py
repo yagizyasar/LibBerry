a52d6fa9ba0d4b8db4cbf5716dc559049eea4606
saitcanbaskol <saitcan.baskol@ug.bilkent.edu.tr>
2022-05-17 17:11:47 +0300

    war

--- LibBerry/warning/urls.py
+++ LibBerry/warning/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.init_warning_list_view, name='warning_creation_root'),
-    path('warningcreation/', views.create_warning, name='warning_creation'),
+    path('warningoverduecreation/', views.create_overdue_warning, name='create_overdue_warning'),
+    path('warningnearduecreation/',views.create_neardue_warning,name='create_nearlydue_warning')
 ]
